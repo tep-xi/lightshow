4d2285ba00ab0a83d29e44af1eeb3c43232447c7
Deven Lahoti <deywos@mit.edu>
2016-10-08 19:54:21 -0400

    put into single file

--- lightshow.py
+++ lightshow.py
@@ -12,8 +12,33 @@ import numpy
 import sys
 import math
 import struct
+import serial
+import time
 
-import lightagain
+ser = serial.Serial('/dev/ttyACM0', 19200, timeout=1)
+
+def lightSwitch(numbers):
+    toSend = [0b00000000, ] * 4
+    if numbers:
+        for light in numbers:
+            if light >= 0 and light < 32:
+                pos = light % 8
+                index = light // 8
+                toSend[index] = toSend[index] | (0b10000000 >> pos)
+            else:
+                toSend = [0b00000000, ] * 4
+    else:
+        toSend = [0b00000000, ] * 4
+    ser.write(toSend)
+
+
+def lightMusic(ls):
+    bass = [7, 10, 16, 18, 23, 26, 27, 28]
+    midOne = [1, 5, 12, 13, 22]
+    midTwo = [15, 2, 3, 4]
+    high = [14, 17, 21, 25, 31]
+    constant = [29]
+    return (bass * ls[0] + midOne * ls[1] + midTwo * ls[2] + high * ls[3] + constant)
 
 inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
 #inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK,device='hw:1,1,0')
@@ -87,4 +112,4 @@ while True:
         print(outstr)
         time.sleep(0.01)
         threshold = [offset[i] + scale[i]*numpy.mean(running[i]) for i in xrange(0,len(running))]
-        lightagain.lightSwitch(lightagain.lightMusic(thresholder(levels,threshold)))
+        lightSwitch(lightMusic(thresholder(levels,threshold)))
