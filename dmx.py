667e16448d2e246f716eccb8c61f4993fbcb9c00
Deven Lahoti <deywos@mit.edu>
2017-03-02 22:08:55 -0500

    add the correct dmx.py

--- dmx.py
+++ dmx.py
@@ -2,116 +2,159 @@
 
 import socket
 import time
-import array
-import struct
-import functools
-import numpy
-try:
-    import numpy
-except ImportError:
-    numpy = False
+import math
 
-def _numpy_badness(*args, **kwargs):
-    raise RuntimeError("Need numpy here")
+KINET_MAGIC=chr(0x04)+chr(0x01)+chr(0xdc)+chr(0x4a)
+KINET_VERSION=chr(0x01)+chr(0x00)
+KINET_TYPE_DMXOUT=chr(0x01)+chr(0x01)
 
-def numpycheck(method):
-    if numpy:
-        return method
-    else:
-        return _numpy_badness
+class DmxConnection :
+    def __init__(self, address, port, dmx_port) :
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+        self.sock.connect((address,port))
+        self.dmx_port = dmx_port
 
+    def send_dmx(self, data) :
+        out=KINET_MAGIC+KINET_VERSION+KINET_TYPE_DMXOUT
+        out+=chr(0x00)+chr(0x00)+chr(0x00)+chr(0x00) #seq
+        out+=chr(self.dmx_port) # dmx port number
+        out+=chr(0x00) #flags
+        out+=chr(0x00)+chr(0x00) # timerVal
+        out+=chr(0xFF)+chr(0xFF)+chr(0xFF)+chr(0xFF) # uni
+        out+=data
+        if(len(out)!=self.sock.send(out)) :
+            print "socket problem"
+            raise SystemExit(1)
 
-class sPDS480ca(object):
+class sPDS480caConnection :
     def __init__(self, address, universe) :
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
         self.sock.connect((address, 6038))
         self.universe = universe
-        self.magic = struct.pack('<IHHIIBHHH',
-                                 1255932164, 1, 264,
-                                 0, 0, universe, 
-                                 209, 0, 2)
+        self.magic = ("\x04\x01\xdc\x4a" # magic number
+                      +"\x01\x00" # kk version
+                      +"\x08\x01"
+                      +"\x00\x00\x00\x00\x00\x00\x00\x00"
+                      +chr(universe)+"\xD1\x00\x00\x00\x02\x00")
 
     def send_dmx(self, data) :
-        self.sock.send(self.magic + data)
+        self.sock.send(self.magic+data)
         # no error detection! yay!
 
-
-
-class RGBLight(object):
-    __slots__ = ('r', 'g', 'b', 'panel', 'row', 'col')
-
-    def __init__(self, row, col, panel=None):
-        self.r = self.g = self.b = 0
+class RGBLight :
+    def __init__(self, row, col) :
+        self.r = 0
+        self.g = 0
+        self.b = 0
         self.row = row
         self.col = col
-        self.panel = panel
-
+    
     # h,s,b are from 0 to 1
     def sethue(self, hue, brightness, saturation) :
         angle = hue*6%6.0
         brightness = min(max(float(brightness), 0.0), 1.0)
         saturation = min(max(float(saturation), 0.0), 1.0)
-        r, g, b = 0, 0, 0
         if angle<2.0 :
-            r=1
+            self.r=1
             if angle<1.0 :
-                g = 0
-                b = 1.0-angle
+                self.g = 0
+                self.b = 1.0-angle
             else :
-                g = angle-1.0
-                b = 0
+                self.g = angle-1.0
+                self.b = 0
         if angle>=2.0 and angle<4.0 :
-            g=1
+            self.g=1
             if angle<3.0 :
-                r=3.0-angle
-                b=0
+                self.r=3.0-angle
+                self.b=0
             else :
-                r=0
-                b=angle-3.0
+                self.r=0
+                self.b=angle-3.0
         if angle>=4.0 :
-            b=1
+            self.b=1
             if angle<5.0 :
-                g=5.0-angle
-                r=0
+                self.g=5.0-angle
+                self.r=0
             else :
-                g=0
-                r=angle-5.0
-        self.r=brightness*(min(max(brightness-saturation, 0.0), 1.0)*r+saturation)
-        self.g=brightness*(min(max(brightness-saturation, 0.0), 1.0)*g+saturation)
-        self.b=brightness*(min(max(brightness-saturation, 0.0), 1.0)*b+saturation)
+                self.g=0
+                self.r=angle-5.0
+        self.r=brightness*(min(max(brightness-saturation, 0.0), 1.0)*self.r+saturation)
+        self.g=brightness*(min(max(brightness-saturation, 0.0), 1.0)*self.g+saturation)
+        self.b=brightness*(min(max(brightness-saturation, 0.0), 1.0)*self.b+saturation)
 
     def setrgb(self, red, green, blue):
         self.r = red
         self.g = green
         self.b = blue
 
-    def __repr__(self):
-        return '<RGBPixel: (%r, %r, %r)>' % (self.r, self.g, self.b)
+    # n.b. color-changing CK lights are not really designed to give off white-ish light
+    # with subtle differences in color temperature (although CK does make some lights
+    # that are designed to provide a wide range of color temperatures) but I wanted to
+    # write this anyway. more deets on this algorithm at
+    # http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/ 
+    # - sinback 2015
+    def settemp(self, temperature, brightness) :
+	# brightness a float between [0, 1]
+	brightness = min(max(brightness, 0.0), 1.0)
+	temperature = min(max(temperature, 1000.0), 40000.0)
+	scaledTemp = temperature/100.0
+	if scaledTemp < 66:
+		self.r = brightness*255.0
+	else:
+		interim = scaledTemp - 60
+		interim = 329.698727446 * (interim ** -0.1332047592)
+		self.r = brightness*min(max(interim, 0.0), 255.0)
 
-class PanelIter(object):
+	if scaledTemp < 66:
+		interim = scaledTemp
+		interim = 99.4708025861 * math.log(interim) - 161.1195681661 
+		self.g = brightness*min(max(interim, 0.0), 255.0)
+	else:
+		interim = scaledTemp - 60
+		interim = 288.1221695283 * (interim ** -0.0755148492) 	
+		self.g = brightness*min(max(interim, 0.0), 255.0)
 
-    @numpycheck
-    def blank_matrix(self):
-        A = numpy.ndarray((self.height, self.width, 3), dtype=numpy.single)
-        A[:] = 0
-        return A
+	if scaledTemp >= 66:
+		self.b = brightness*255.0
+	elif scaledTemp <= 19:
+		self.b = brightness*0.0
+	else:
+		interim = scaledTemp - 10
+		interim = 138.5177312231 * math.log(interim) - 305.0447927307 
+		self.b = brightness*min(max(interim, 0.0), 255.0)
 
-    @numpycheck
-    def sethue(self, arr):
-        for row in self.lights:
-            for pixel in row:
-                pixel.sethue(*arr[pixel.row, pixel.col])
+	self.r = self.r/255.0
+	self.g = self.g/255.0
+	self.b = self.b/255.0
+	
 
-    @numpycheck
-    def setrgb(self, arr):
-        for row in self.lights:
-            for pixel in row:
-                pixel.setrgb(*arr[pixel.row, pixel.col])
-
-class LightPanel(PanelIter):
-    changed = True
+class SimpleLights :
+    def __init__(self, dmx) :
+        self.lights = [[RGBLight(j, 0)] for j in range(0, 128)]
+        self.dmx = dmx
+        self.width = 1
+        self.height = 128
+        self.time = time.time()
+    def output(self) :
+        out = chr(0x00)
+        for i in range(0,128) :
+            out += chr(int(255*min(max(float(self.lights[i][0].r),0),1.0)))
+            out += chr(int(255*min(max(float(self.lights[i][0].g),0),1.0)))
+            out += chr(int(255*min(max(float(self.lights[i][0].b),0),1.0)))
+        while(len(out)<512) :
+            out += chr(0x00)
+        out += chr(255)+chr(191)
+        self.dmx.send_dmx(out)
+    def outputAndWait(self, fps) :
+        self.output()
+        endtime = time.time()-self.time
+        if(1.0/fps > endtime) :
+            time.sleep(1.0/fps-endtime)
+        self.time = time.time()
+        
+class LightPanel :
     def __init__(self, dmx, comp) :
-        self.lights = [[RGBLight(j, i, self) for i in range(0,12)]
+        self.lights = [[RGBLight(j, i) for i in range(0,12)]
                        for j in range(0,12)]
         self.dmx = dmx
         self.width = 12
@@ -137,7 +180,6 @@ class LightPanel(PanelIter):
             out+=chr(0x00)
         out+=chr(255)+chr(191)
         self.dmx.send_dmx(out)
-        self.changed = False
 
     def outputAndWait(self, fps) :
         self.output()
@@ -146,53 +188,42 @@ class LightPanel(PanelIter):
             time.sleep(1.0/fps-endtime)
         self.time = time.time()
 
-class HalfLightPanel(PanelIter):
-    changed = True
+class HalfLightPanel :
     # direction: 0 is "bottom right corner is (0,0)" and 1 is "bottom left ..."
     def __init__(self, dmx, direction) :
-        self.outputdata = array.array('B', (0,) * 657)
         self.width = 6
         self.height = 12
         self.direction = direction
-        self.lights = [[RGBLight(j, i, self) for i in range(0, self.width)]
+        self.lights = [[RGBLight(j, i) for i in range(0, self.width)]
                        for j in range(0, self.height)]
         self.dmx = dmx
         self.time = time.time()
-
-    def output(self, wait_fps = None) :
+    def output(self) :
+        out = chr(0)
         row = 0
         col = 0
-        ptr = 1
-        output = self.outputdata
         for i in range(0, 72) :
-            row = i % 12
+            row = i%12
             if self.direction == 0 :
                 col = 5-(i//12)
             else :
                 col = i//12
             l = self.lights[row][col]
-            output[ptr] = int(255*min(max(l.r ** 0.9, 0.0), 1.0))
-            output[ptr + 1] = int(255*min(max(l.g ** 0.9, 0.0), 1.0))
-            output[ptr + 2] = int(255*min(max(l.b ** 0.9, 0.0), 1.0))
-            ptr += 3
-        output[-1] = 191
-        #if wait_fps:
-        #    self._wait(wait_fps)
-        self.dmx.send_dmx(output.tostring())
-        self.changed = False
-
-    def _wait(self, fps):
-        curtime = time.time()
-        endtime = curtime - self.time
-        if 1.0 / fps > endtime:
-            time.sleep(1.0 / fps - endtime)
-        self.time = time.time()
-
+            out += chr(int(255*min(max(pow(l.r, 0.9), 0.0), 1.0)))
+            out += chr(int(255*min(max(pow(l.g, 0.9), 0.0), 1.0)))
+            out += chr(int(255*min(max(pow(l.b, 0.9), 0.0), 1.0)))
+        for i in range(12*6, 511) :
+            out += chr(0)
+        out += chr(0xbf)
+        self.dmx.send_dmx(out)
     def outputAndWait(self, fps) :
         self.output()
-        self._wait(fps)
+        endtime = time.time()-self.time
+        if(1.0/fps > endtime) :
+            time.sleep(1.0/fps-endtime)
+        self.time = time.time()
 
-class PanelComposite(PanelIter):
+class PanelComposite :
     def __init__(self) :
         self.panels = []
         self.panelloc = []
@@ -205,8 +236,8 @@ class PanelComposite(PanelIter):
         self.width=max(self.width, llcol+panel.width)
         self.height=max(self.height, llrow+panel.height)
         newlights = [[RGBLight(row, col) for col in range(self.width)] for row in range(self.height)]
-        for row in self.lights:
-            for light in row:
+        for row in self.lights :
+            for light in row :
                 newlights[light.row][light.col] = light
         for row in panel.lights :
             for light in row :
@@ -215,7 +246,7 @@ class PanelComposite(PanelIter):
                 newlights[light.row][light.col] = light
         self.lights = newlights
     def output(self) :
-        for panel in self.panels:
+        for panel in self.panels :
             panel.output()
     def outputAndWait(self, fps) :
         t = False
@@ -227,32 +258,22 @@ class PanelComposite(PanelIter):
 
 def getDefaultPanel() :
     panel = PanelComposite()
-    for i in range(1,31) :
-        ip, x, y = _get_panel_info(i)
-        num = ((i - 1) & 15) + 1
-        panel_part = HalfLightPanel(sPDS480caConnection(ip, num), 1-(i%2))
-        panel.addPanel(panel_part, y, x)
+    for i in range(1,17) :
+        panel_part = HalfLightPanel(sPDS480caConnection("18.224.0.194", i), 1-(i%2))
+        panel.addPanel(panel_part, 12*(1-((i-1)//8)), 6*((i-1)%8))
     return panel
 
-def _get_panel_info(i):
-    ips = {0: '18.102.224.46',
-           1: '18.102.224.47',}
-    ip = ips[i // 17]
-    x, y = 6 * ((i - 1) % 10), 12 * ((30 - i) // 10)
-    return ip, x, y
-
+#def getDefaultPanel() :
+#    return LightPanel(DmxConnection("18.224.1.163", 6038, 0), 0)
 
 if __name__=="__main__" :
     a = getDefaultPanel()
-    color = 1
-    import sys
-    while True:
-
-        for row in a.lights:
-            for light in row:
-                light.setrgb(255, 0, 0)
+    color = 1.0
+    while True :
+        for row in a.lights :
+            for light in row :
+                light.r=color
                 a.outputAndWait(30)
-        #sys.exit()
         for row in a.lights :
             for light in row :
                 light.g=color
